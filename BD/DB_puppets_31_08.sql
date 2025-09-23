67240e8ab01911da8976c72a70f2aa1337c99d39
Gus9555 <160183960+Gus9555@users.noreply.github.com>
2025-09-22 19:41:02 -0600

    base datos
    la base de datos

--- BD/DB_puppets_31_08.sql
+++ BD/DB_puppets_31_08.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 01, 2025 at 05:51 AM
--- Server version: 9.4.0
--- PHP Version: 8.4.11
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-09-2025 a las 03:40:24
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u445672402_puppets`
+-- Base de datos: `u445672402_puppets`
 --
 CREATE DATABASE IF NOT EXISTS `u445672402_puppets` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `u445672402_puppets`;
@@ -26,25 +26,24 @@ USE `u445672402_puppets`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `appointments`
+-- Estructura de tabla para la tabla `appointments`
 --
 
-DROP TABLE IF EXISTS `appointments`;
 CREATE TABLE `appointments` (
-  `id` bigint UNSIGNED NOT NULL,
-  `full_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pet_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `pet_type` varchar(255) NOT NULL,
   `appointment_date` date NOT NULL,
   `appointment_time` time NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci,
+  `message` text DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `appointments`
+-- Volcado de datos para la tabla `appointments`
 --
 
 INSERT INTO `appointments` (`id`, `full_name`, `email`, `phone`, `pet_type`, `appointment_date`, `appointment_time`, `message`, `created_at`, `updated_at`) VALUES
@@ -54,61 +53,83 @@ INSERT INTO `appointments` (`id`, `full_name`, `email`, `phone`, `pet_type`, `ap
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cache`
+-- Estructura de tabla para la tabla `cache`
 --
 
-DROP TABLE IF EXISTS `cache`;
 CREATE TABLE `cache` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `expiration` int NOT NULL
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cache_locks`
+-- Estructura de tabla para la tabla `cache_locks`
 --
 
-DROP TABLE IF EXISTS `cache_locks`;
 CREATE TABLE `cache_locks` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `owner` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `expiration` int NOT NULL
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jobs`
+-- Estructura de tabla para la tabla `citas`
 --
 
-DROP TABLE IF EXISTS `jobs`;
+CREATE TABLE `citas` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `pet_id` bigint(20) UNSIGNED NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `mensaje` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`id`, `user_id`, `pet_id`, `fecha`, `hora`, `mensaje`, `created_at`, `updated_at`) VALUES
+(1, 9, 4, '2025-09-26', '08:20:00', 'es mio', '2025-09-11 14:16:04', '2025-09-11 14:16:04'),
+(2, 9, 3, '2025-09-20', '08:43:00', 'es mio', '2025-09-11 14:43:28', '2025-09-11 14:43:28'),
+(3, 10, 6, '2025-09-12', '08:34:00', 'es nervisos, Sesión de nudos', '2025-09-11 19:34:12', '2025-09-11 19:34:12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jobs`
+--
+
 CREATE TABLE `jobs` (
-  `id` bigint UNSIGNED NOT NULL,
-  `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `attempts` tinyint UNSIGNED NOT NULL,
-  `reserved_at` int UNSIGNED DEFAULT NULL,
-  `available_at` int UNSIGNED NOT NULL,
-  `created_at` int UNSIGNED NOT NULL
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Estructura de tabla para la tabla `migrations`
 --
 
-DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE `migrations` (
-  `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int NOT NULL
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `migrations`
+-- Volcado de datos para la tabla `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -121,115 +142,126 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pets`
+-- Estructura de tabla para la tabla `pets`
 --
 
-DROP TABLE IF EXISTS `pets`;
 CREATE TABLE `pets` (
-  `id` bigint UNSIGNED NOT NULL,
-  `user_id` bigint UNSIGNED NOT NULL,
-  `especie` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `raza` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `color` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `especie` varchar(255) NOT NULL,
+  `raza` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `color` varchar(255) NOT NULL,
   `fecha_nacimiento` date NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `pets`
+-- Volcado de datos para la tabla `pets`
 --
 
 INSERT INTO `pets` (`id`, `user_id`, `especie`, `raza`, `nombre`, `color`, `fecha_nacimiento`, `created_at`, `updated_at`) VALUES
 (1, 6, 'Perro', 'Beagle', 'Brady', 'beige', '2025-08-01', '2025-09-01 11:39:11', '2025-09-01 11:39:11'),
-(2, 6, 'Perro', 'Bulldog Francés', 'Orko', 'Cerbato', '2023-02-23', '2025-09-01 11:44:27', '2025-09-01 11:44:27');
+(2, 6, 'Perro', 'Bulldog Francés', 'Orko', 'Cerbato', '2023-02-23', '2025-09-01 11:44:27', '2025-09-01 11:44:27'),
+(3, 9, 'Perro', 'Chihuahua', 'nunu', 'verdoso', '2025-08-22', '2025-09-11 09:31:46', '2025-09-11 09:31:46'),
+(4, 9, 'Gato', 'Mestizo', 'gato', 'tipo naranjoso con verde', '2025-08-15', '2025-09-11 12:34:53', '2025-09-11 12:34:53'),
+(5, 9, 'Perro', 'Pastor Alemán', 'Hus', 'ddd', '2025-09-13', '2025-09-11 13:59:24', '2025-09-11 13:59:24'),
+(6, 10, 'Perro', 'Pastor Alemán', 'kk', 'negro', '2025-11-15', '2025-09-11 19:33:27', '2025-09-11 19:33:27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sessions`
+-- Estructura de tabla para la tabla `sessions`
 --
 
-DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE `sessions` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` bigint UNSIGNED DEFAULT NULL,
-  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_agent` text COLLATE utf8mb4_unicode_ci,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_activity` int NOT NULL
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estructura de tabla para la tabla `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` bigint UNSIGNED NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `created_at`, `updated_at`) VALUES
 (6, 'davidernestoagreda@gmail.com', 'dagreda_12', '$2y$12$grCdxewvzYO3G9ICtyj4ru4lPp0Yo.qOnTgYOQW3NkRqY0JOKN1DW', '2025-08-31 13:13:07', '2025-08-31 13:13:07'),
 (7, 'davidagreda@gmail.com', 'dagreda12', '$2y$12$5g241GjlSI3TjZd9HiC1.OeOg8WVGDEQfwfuGQ6KrWYQt41985t0G', '2025-08-31 13:14:04', '2025-08-31 13:14:04'),
-(8, 'yohalmocruz@gmail.com', 'yohalmo', '$2y$12$wXnYV04s9IlJ0fMrnW/BA.P8EcFg9dJ7LK1SR7qWS98Gxrph9qASG', '2025-09-01 09:58:32', '2025-09-01 09:58:32');
+(8, 'yohalmocruz@gmail.com', 'yohalmo', '$2y$12$wXnYV04s9IlJ0fMrnW/BA.P8EcFg9dJ7LK1SR7qWS98Gxrph9qASG', '2025-09-01 09:58:32', '2025-09-01 09:58:32'),
+(9, 'gustavomcastillo120@gmail.com', 'usuario', '$2y$12$UxqGQPkR1vJqMr8ChVa0aO4vd7NyEOSSP4zAoZWZUT0fHAKCTHRkC', '2025-09-11 06:06:54', '2025-09-11 06:06:54'),
+(10, 'thecastillo120@gmail.com', 'castillo', '$2y$12$qYq3b2EBCv7Q/neuquRFnexdWIowbuzo40AD.Os43oX1TESbZ.ylK', '2025-09-11 06:15:32', '2025-09-11 06:15:32');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `appointments`
+-- Indices de la tabla `appointments`
 --
 ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `cache`
+-- Indices de la tabla `cache`
 --
 ALTER TABLE `cache`
   ADD PRIMARY KEY (`key`);
 
 --
--- Indexes for table `cache_locks`
+-- Indices de la tabla `cache_locks`
 --
 ALTER TABLE `cache_locks`
   ADD PRIMARY KEY (`key`);
 
 --
--- Indexes for table `jobs`
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `citas_user_id_foreign` (`user_id`),
+  ADD KEY `citas_pet_id_foreign` (`pet_id`);
+
+--
+-- Indices de la tabla `jobs`
 --
 ALTER TABLE `jobs`
   ADD PRIMARY KEY (`id`),
   ADD KEY `jobs_queue_index` (`queue`);
 
 --
--- Indexes for table `migrations`
+-- Indices de la tabla `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `pets`
+-- Indices de la tabla `pets`
 --
 ALTER TABLE `pets`
   ADD PRIMARY KEY (`id`),
   ADD KEY `pets_user_id_foreign` (`user_id`);
 
 --
--- Indexes for table `sessions`
+-- Indices de la tabla `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
@@ -237,51 +269,64 @@ ALTER TABLE `sessions`
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
 --
--- Indexes for table `users`
+-- Indices de la tabla `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `appointments`
+-- AUTO_INCREMENT de la tabla `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `jobs`
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `pets`
+-- AUTO_INCREMENT de la tabla `pets`
 --
 ALTER TABLE `pets`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `pets`
+-- Filtros para la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD CONSTRAINT `citas_pet_id_foreign` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `citas_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `pets`
 --
 ALTER TABLE `pets`
   ADD CONSTRAINT `pets_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
